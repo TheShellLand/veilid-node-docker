2dbcc77ecf93ffef2d1f77990cfd3a688066bcf1
Eli Smith <eli@wavv.com>
2023-09-22 17:56:01 -0600

    Fixed other files with same typo

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $(dirnamr $0); set -xe
+cd $(dirname $0); set -xe
 # build
 
 docker build -t veilid-node:latest .
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # run veilid
 
-cd $(dirnamr $0); set -xe
+cd $(dirname $0); set -xe
 
 docker rm -f veilid-node || true
 docker run -d --name veilid-node veilid-node:latest .
