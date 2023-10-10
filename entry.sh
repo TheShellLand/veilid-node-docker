afbc9df0286d3b1288ccbb4ee5a31d76586761b6
naisanzaa <naisanza@gmail.com>
2023-10-10 16:28:46 -0500

    Add stdout and stderr logging
    but there's no console output from the service

--- entry.sh
+++ entry.sh
@@ -5,5 +5,9 @@
 set -xe
 
 id
+cd
+RUST_BACKTRACE=1 RUST_BACKTRACE=full COLORBT_SHOW_HIDDEN=1 veilid-server -s logging.system.enabled=false > veilid.log 2> veilid-errors.log &
 
-RUST_BACKTRACE=1 RUST_BACKTRACE=full COLORBT_SHOW_HIDDEN=1 veilid-server
+jobs
+
+tail -f veilid.log veilid-errors.log
