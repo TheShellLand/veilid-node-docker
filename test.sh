4463b1caf0ddf377f45b4fdf7ae5149ab962fa18
naisanzaa <naisanza@gmail.com>
2023-11-24 21:40:39 -0800

    update test

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@
 
 cd $(dirname $0); set -xe
 
-docker run --rm -it ghcr.io/theshellland/veilid-node:latest $@
+docker run --rm -it -p 8080:80 -p 4443:443 -p 5959:5959 -p 4317:4317 ghcr.io/theshellland/veilid-node:latest $@
