282a6cb2496d280d801d12be81bdb226a0fa9ead
naisanzaa <naisanza@gmail.com>
2023-11-26 15:46:53 -0800

    persist veilid data

--- run.sh
+++ run.sh
@@ -8,7 +8,18 @@ docker network create veilid || true
 docker rm -f veilid-node 2>/dev/null || true
 
 if [ -z "$@" ]; then
-  docker run -d --name veilid-node --net veilid ghcr.io/theshellland/veilid-node:latest --set-config "client_api.listen_address='0.0.0.0:5959'" $@
+  docker run -d --name veilid-node --net veilid \
+  -v veilid-server-config:/config \
+  -v veilid-server-node://root/.local/share/veilid/ \
+  -v veilid-server-logs:/logs \
+  -v veilid-server-data:/var/db/veilid-server \
+  ghcr.io/theshellland/veilid-node:latest --set-config 'client_api.listen_address="0.0.0.0:5959"' $@
 else
-  docker run -d --name veilid-node --net veilid -p 5959:5959 ghcr.io/theshellland/veilid-node:latest $@
+  docker run -d --name veilid-node --net veilid \
+  -v veilid-server-config:/config \
+  -v veilid-server-node://root/.local/share/veilid/ \
+  -v veilid-server-logs:/logs \
+  -v veilid-server-data:/var/db/veilid-server \
+  -p 5959:5959 \
+  ghcr.io/theshellland/veilid-node:latest $@
 fi
