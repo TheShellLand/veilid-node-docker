6e945b5b294eabb19f620ba72fda0dc155bc1fbc
naisanzaa <naisanza@gmail.com>
2024-08-04 00:45:33 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ docker network create veilid || true
 
 if [ "$@" == "--dump-config" ]; then
   docker run --rm --net veilid \
-  -v veilid-server-config:/root/.config/veilid/ \
+  -v veilid-server-config:/config \
   -v veilid-server-node:/root/.local/share/veilid/ \
   -v veilid-server-logs:/logs \
   -v veilid-server-data:/var/db/veilid-server \
@@ -17,25 +17,27 @@ if [ "$@" == "--dump-config" ]; then
 elif [ -z "$@" ]; then
   docker rm -f veilid-server 2>/dev/null || true
   docker run -d --name veilid-server --net veilid \
-  -v veilid-server-config:/root/.config/veilid/ \
+  -v veilid-server-config:/config \
   -v veilid-server-node:/root/.local/share/veilid/ \
   -v veilid-server-logs:/logs \
   -v veilid-server-data:/var/db/veilid-server \
-  -e DEBUG=true \
+  -e DEBUG=false \
   --restart always \
   ghcr.io/theshellland/veilid-node:latest \
   "$@"
 else
   docker rm -f veilid-server 2>/dev/null || true
   docker run -d --name veilid-server --net veilid \
-  -v veilid-server-config:/root/.config/veilid/ \
+  -v veilid-server-config:/config \
   -v veilid-server-node:/root/.local/share/veilid/ \
   -v veilid-server-logs:/logs \
   -v veilid-server-data:/var/db/veilid-server \
-  -e DEBUG=true \
+  -e DEBUG=false \
   -p 5959:5959 \
   -p 5150:5150 \
   --restart always \
   ghcr.io/theshellland/veilid-node:latest \
     $@
 fi
+
+docker ps -a | grep veilid-server
