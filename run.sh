0ecc45795095f0ce8feba58a562cccf30e70bbe3
naisanza <naisanza@gmail.com>
2025-02-20 20:14:20 -0600

    [docker] updated scripts

--- run.sh
+++ run.sh
@@ -5,24 +5,14 @@
 cd $(dirname $0); set -xe
 
 docker network create veilid || true
+docker rm -f veilid-server 2>/dev/null || true
 
-if [ "$@" == "--dump-config" ]; then
-  docker run --rm --net veilid \
-  -v veilid-server-config:/config \
-  -v veilid-server-node:/root/.local/share/veilid/ \
-  -v veilid-server-logs:/logs \
-  -v veilid-server-data:/var/db/veilid-server \
-  ghcr.io/theshellland/veilid-node:latest \
-  --dump-config
-else
-  docker rm -f veilid-server 2>/dev/null || true
-  docker run -d --name veilid-server --net veilid \
-  -v veilid-server-config:/config \
-  -v veilid-server-node:/root/.local/share/veilid/ \
-  -v veilid-server-logs:/logs \
-  -v veilid-server-data:/var/db/veilid-server \
-  -e DEBUG=false \
-  --restart always \
-  ghcr.io/theshellland/veilid-node:latest \
-  "$@"
-fi
+docker run --name veilid-server --net veilid \
+-v veilid-server-config:/config \
+-v veilid-server-node:/root/.local/share/veilid/ \
+-v veilid-server-logs:/logs \
+-v veilid-server-data:/var/db/veilid-server \
+-e DEBUG=false \
+--restart always \
+ghcr.io/theshellland/veilid-node:latest \
+"$@"
--- test-config.sh
+++ test-config.sh
@@ -6,6 +6,7 @@ cd $(dirname $0); set -xe
 
 docker network create veilid || true
 docker rm -f veilid-server 2>/dev/null || true
+
 docker run \
   --rm \
   -it \
